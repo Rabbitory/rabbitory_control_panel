6dbcb909ddc41fda47648edb0317b6b0645756a0
jim <zijin@ualberta.ca>
2025-03-30 14:34:16 -0700

    Diplay user/password and endpoint

--- src/utils/dynamoDBUtils.ts
+++ src/utils/dynamoDBUtils.ts
@@ -1,8 +1,12 @@
 import { DynamoDBClient, DeleteItemCommand } from "@aws-sdk/client-dynamodb";
-import { PutCommand, DynamoDBDocumentClient, } from "@aws-sdk/lib-dynamodb";
+import {
+  PutCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 interface data {
-  [key: string]: string
+  [key: string]: string;
 }
 
 export const storeToDynamoDB = async (
@@ -16,34 +20,55 @@ export const storeToDynamoDB = async (
   try {
     const command = new PutCommand({
       TableName: tableName,
-      Item: data
-    })
+      Item: data,
+    });
 
     const response = await docClient.send(command);
-    console.log("data successfully written to DynamoDB")
+    console.log("data successfully written to DynamoDB");
     return response;
   } catch (err) {
     console.error("Error writing to DynamoDB:", err);
     throw new Error("Failed to store data to DynamoDB");
   }
-}
+};
+
+export const fetchFromDynamoDB = async (
+  tableName: string,
+  partitionKey: { [key: string]: string }
+) => {
+  const client = new DynamoDBClient({ region: process.env.REGION });
+  const docClient = DynamoDBDocumentClient.from(client);
+  try {
+    console.log("Attempting to fetch from DynamoDB...");
+    const command = new GetCommand({
+      TableName: tableName,
+      Key: partitionKey,
+    });
+    const response = await docClient.send(command);
+    console.log("Item fetched successfully!");
+    return response;
+  } catch (err) {
+    console.error("Error fetching item:", err);
+    throw new Error("Failed to fetch data from DynamoDB");
+  }
+};
 
 export const deleteFromDynamoDB = async (
   tableName: string,
-  partitionKey: { [key: string]: { S: string } },
+  partitionKey: { [key: string]: { S: string } }
 ) => {
-  const client = new DynamoDBClient({ region: process.env.REGION })
+  const client = new DynamoDBClient({ region: process.env.REGION });
 
   try {
-    console.log("Attempting to delete from DynamoDB...")
+    console.log("Attempting to delete from DynamoDB...");
     const command = new DeleteItemCommand({
       TableName: tableName,
-      Key: partitionKey
-    })
+      Key: partitionKey,
+    });
 
     const response = await client.send(command);
     console.log("Item deleted successfully:", response);
   } catch (err) {
     console.error("Error deleting item:", err);
   }
-}
+};
--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,12 +1,47 @@
-import * as argon2 from "argon2";
+// import * as argon2 from "argon2";
 
-const encrypt = async (data: string): Promise<string | null> => {
-  try {
-    return await argon2.hash(data);
-  } catch (err) {
-    console.error("Error encrypting data:", err);
-    return null;
-  }
-}
+// const encrypt = async (data: string): Promise<string | null> => {
+//   try {
+//     return await argon2.hash(data);
+//   } catch (err) {
+//     console.error("Error encrypting data:", err);
+//     return null;
+//   }
+// }
 
-export default encrypt;
+// export default encrypt;
+import crypto from "crypto";
+
+const algorithm = "aes-256-cbc";
+
+const secretKey =
+  process.env.ENCRYPTION_SECRET || "your_default_secret_key_here";
+const key = crypto
+  .createHash("sha256")
+  .update(String(secretKey))
+  .digest()
+  .slice(0, 32);
+
+export const encrypt = (text: string): string => {
+  // Create a random Initialization Vector.
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const encrypted = Buffer.concat([
+    cipher.update(text, "utf8"),
+    cipher.final(),
+  ]);
+
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
+};
+
+export const decrypt = (encryptedText: string): string => {
+  const textParts = encryptedText.split(":");
+  const iv = Buffer.from(textParts.shift() as string, "hex");
+  const encrypted = Buffer.from(textParts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  const decrypted = Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final(),
+  ]);
+  return decrypted.toString("utf8");
+};

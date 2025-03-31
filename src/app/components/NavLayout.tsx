73486c915e9773a3dc3d31d772137cc1b4946bde
Mason <31372737+Ovaculos@users.noreply.github.com>
2025-03-31 16:06:11 -0500

    region query param to `Overview` button

--- src/app/components/NavLayout.tsx
+++ src/app/components/NavLayout.tsx
@@ -12,7 +12,9 @@ export default function NavLayout({ name }: NavLayoutProps) {
   return (
     <nav className="w-70 bg-gray-200 p-4 min-h-screen pl-10 pr-10">
       <h1 className="text-xl font-semibold mt-8 mb-8 hover:text-gray-700">
-        <Link href={`/instances/${name}`}>Overview</Link>
+        <Link href={`/instances/${name}?region=${instance?.region}`}>
+          Overview
+        </Link>
       </h1>
       <ul>
         <li className="mb-4">

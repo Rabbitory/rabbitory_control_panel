e1375317fb8c0e2fe5000516c2ce6fc008079d6c
Mason <31372737+Ovaculos@users.noreply.github.com>
2025-04-02 10:24:17 -0500

    Removed useEffect

--- src/app/instances/[name]/alarms/new/page.tsx
+++ src/app/instances/[name]/alarms/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useInstanceContext } from "../../InstanceContext";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 export default function AlarmsPage() {

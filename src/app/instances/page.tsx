c7544768847fdf8bcd78fe9d4b84a81316d7aae9
larencozart <larencozart@gmail.com>
2025-04-09 00:53:58 -0500

    Remove space

--- src/app/instances/page.tsx
+++ src/app/instances/page.tsx
@@ -31,21 +31,18 @@ export default function Home() {
       <div className="flex justify-between items-center mb-6">
         <h1 className="font-heading1 text-2xl text-headertext1">Instances</h1>
         <Link href="/instances/newForm">
-  <button
-    className={`
-      font-heading1 font-semibold py-2 px-6 bg-btn1
-      text-mainpage1 rounded-sm hover:bg-btnhover1 transition-all duration-200
-      text-md
-      hover:shadow-[0_0_8px_3px_rgba(135,217,218,0.5)] /* Softer glow */
-      ${!isLoading && instances.length === 0 ? 'pulse-glow' : ''}
-    `}
-  >
-    + Create New Instance
-  </button>
-</Link>
-
-
-
+          <button
+            className={`
+              font-heading1 font-semibold py-2 px-6 bg-btn1
+              text-mainpage1 rounded-sm hover:bg-btnhover1 transition-all duration-200
+              text-md
+              hover:shadow-[0_0_8px_3px_rgba(135,217,218,0.5)] /* Softer glow */
+              ${!isLoading && instances.length === 0 ? 'pulse-glow' : ''}
+            `}
+          >
+            + Create New Instance
+          </button>
+        </Link>
       </div>
 
       <table className="table-fixed w-full text-sm font-text1 border-separate border-spacing-y-3">

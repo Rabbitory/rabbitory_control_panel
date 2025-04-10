2a7a783ff9fd956de40d3904b24ffca4fd1a3f6f
Mason <31372737+Ovaculos@users.noreply.github.com>
2025-04-10 13:34:24 -0500

    Fixed userData

--- src/utils/AWS/AMI/AMI.ts
+++ src/utils/AWS/AMI/AMI.ts
@@ -1,12 +1,10 @@
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
 export default async function getUbuntuAmiId(
-  instanceType: string,
+  architecture: "amd64" | "arm64",
   region = process.env.REGION,
 ) {
   const client = new SSMClient({ region });
-
-  const architecture = instanceType.includes("t2.") ? "amd64" : "arm64";
 
   const parameterName = `/aws/service/canonical/ubuntu/server/24.04/stable/current/${architecture}/hvm/ebs-gp3/ami-id`;
   const command = new GetParameterCommand({ Name: parameterName });

ce5be5561202353c15a5fbf5cf26829ba878ad08
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-04-11 12:58:33 -0500

    slight change to message

--- src/app/instances/[name]/alarms/page.tsx
+++ src/app/instances/[name]/alarms/page.tsx
@@ -103,7 +103,8 @@ export default function AlarmsPage() {
     <div className="max-w-4xl mx-auto p-6 bg-card text-pagetext1 rounded-sm shadow-md mt-8">
       <h1 className="font-heading1 text-headertext1 text-2xl mb-10">Alarms</h1>
       <p className="font-text1 text-sm mb-6">
-        Alarms are triggered when the storage or memory usage of your RabbitMQ instance exceeds the specified thresholds. You can set up alarms to receive notifications via Slack when these thresholds are reached.
+        Alarms are triggered when the storage or memory usage of your RabbitMQ instance exceeds the specified thresholds.
+        You can set up alarms to receive notifications via Slack using the button below.
       </p>
       {isFetching ? (
         <div>Loading...</div>

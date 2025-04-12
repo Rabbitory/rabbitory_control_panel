8ee34afa98bf4ffbb0bd8176f28cb71a386bb99a
larencozart <larencozart@gmail.com>
2025-04-12 12:42:10 -0500

    Add general page loading skeleton

--- src/app/components/InstanceContent.tsx
+++ src/app/components/InstanceContent.tsx
@@ -7,6 +7,34 @@ import { useInstanceContext } from "../instances/[name]/InstanceContext";
 import axios from "axios";
 import { Instance } from "@/types/instance";
 
+export default function PageSkeleton() {
+  return (
+    <div className="flex">
+      {/* Sidebar Skeleton */}
+      <div className="w-[200px] h-[calc(100vh-88px)] sticky top-[88px] bg-mainbg1 border-r-[0.5px] border-border1 p-4 space-y-4">
+          <div className="h-150 bg-gray-600 rounded-md animate-pulse"
+          />
+      </div>
+
+      {/* Main Content Skeleton */}
+      <div className="flex-1 h-[calc(100vh-88px)] overflow-y-auto">
+        <section className="p-4 mb-15">
+          <div className="h-142 text-pagetext1 flex-1 max-w-7xl mx-auto p-6 bg-card rounded-sm shadow-md mt-8 space-y-4">
+            <div className="h-8 bg-gray-600 rounded-md w-1/3 animate-pulse mb-15" />
+
+            <div className="h-5 bg-gray-600 rounded-md w-1/3 animate-pulse mb-4" />
+            <div className="h-20 bg-gray-600 rounded-md w-full animate-pulse mb-10" />
+
+            <div className="h-5 bg-gray-600 rounded-md w-1/3 animate-pulse mb-4" />
+            <div className="h-20 bg-gray-600 rounded-md w-full animate-pulse" />
+
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
+
 export function InstanceContent({
   children,
   name,
@@ -15,13 +43,13 @@ export function InstanceContent({
   name: string;
 }) {
   const { setInstance } = useInstanceContext();
-  const [isFetching, setIsFetching] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const searchParams = useSearchParams();
   const region = searchParams.get("region");
 
   useEffect(() => {
     const fetchInstance = async () => {
-      setIsFetching(true);
+      setIsLoading(true);
       try {
         const response = await axios.get<Instance>(
           `/api/instances/${name}?region=${region}`,
@@ -30,19 +58,16 @@ export function InstanceContent({
       } catch (error) {
         console.error("Error fetching instance:", error);
       } finally {
-        setIsFetching(false);
+        setIsLoading(false);
       }
     };
 
     fetchInstance();
   }, [name, setInstance, region]);
 
-  if (isFetching) {
-    return <div>Loading...</div>;
-  }
-
-
-  return (
+  return isLoading ? (
+    <PageSkeleton />
+  ) : (
     <div className="flex">
       {/* Sidebar Nav */}
       <div className="w-[200px] h-[calc(100vh-88px)] sticky top-[88px] bg-mainbg1 border-r-[0.5px] border-border1">
@@ -51,11 +76,10 @@ export function InstanceContent({
   
       {/* Scrollable Main Content */}
       <div className="flex-1 h-[calc(100vh-88px)] overflow-y-auto">
-        <section className="p-4 mb-15">
-          {children}
-        </section>
+        <section className="p-4 mb-15">{children}</section>
       </div>
     </div>
   );
+  
   
 }

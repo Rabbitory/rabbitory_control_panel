1a09ae109f45a9d5ff6735d7572d78a2f31143a6
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-04-14 11:09:42 -0500

    show webhook url on alarm page, format in table style

--- src/app/instances/[name]/alarms/page.tsx
+++ src/app/instances/[name]/alarms/page.tsx
@@ -104,7 +104,24 @@ export default function AlarmsPage() {
       {/* Slack Endpoint Card */}
       <div className="max-w-4xl mx-auto p-6 bg-card text-pagetext1 rounded-sm shadow-md mt-8">
         <h1 className="font-heading1 text-headertext1 text-2xl mb-10">Slack Endpoint</h1>
-        <p className="font-text1 text-sm mb-6">To create an alarm, setup slack first.</p>
+        {webhookUrl ? (
+          <table className="text-sm w-full table-auto mb-6">
+            <colgroup>
+              <col className="w-1/5" />
+              <col />
+            </colgroup>
+            <tbody className="font-text1">
+              <tr>
+                <td className="py-2">Webhook URL:</td>
+                <td className="py-2">{webhookUrl}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p className="font-text1 text-sm mb-6">
+            You must set up a slack endpoint before creating alarms.
+          </p>
+        )}
         <div className="font-heading1 text-sm flex justify-end gap-4 mt-6">
           <button
             className="px-4 py-2 bg-card border-1 border-btn1 text-btn1 rounded-sm text-center hover:shadow-[0_0_8px_#87d9da] transition-all duration-200 hover:bg-card"
@@ -247,7 +264,6 @@ export default function AlarmsPage() {
 
                 <div className="font-heading1 text-sm flex justify-end gap-4 mt-6">
                   <button
-                    disabled={!webhookUrl}
                     className="px-4 py-2 bg-btn1 hover:bg-btnhover1 text-sm text-mainbg1 font-semibold rounded-sm flex items-center justify-center hover:shadow-[0_0_10px_#87d9da] transition-all duration-200"
                     onClick={(e) => {
                       e.preventDefault();
